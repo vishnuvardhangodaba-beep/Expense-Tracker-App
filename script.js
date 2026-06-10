602905e2ab0c1e794cfb8d722c1692345af16a0b
vishnuvardhangodaba-beep <vishnuvardhangodaba@gmail.com>
2026-06-10 18:33:47 +0530

    expense tracker upgraded

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+let myChart;
 let transactions = [];
 
 function addTransaction() {
 
-let text = document.getElementById("text").value;
-let amount = document.getElementById("amount").value;
+let category = document.getElementById("category");
+let amount = document.getElementById("amount");
 
-if(text === "" || amount === ""){
-alert("Please enter all fields");
+if(amount.value === ""){
+alert("Please enter amount");
 return;
+}
+
+let value = Number(amount.value);
+
+if(category.value !== "Salary"){
+value = -Math.abs(value);
 }
 
 let transaction = {
 id: Date.now(),
-text: text,
-amount: Number(amount)
+text: category.value,
+amount: value
 };
 
 transactions.push(transaction);
@@ -21,8 +28,9 @@ transactions.push(transaction);
 updateUI();
 saveToLocalStorage();
 
-document.getElementById("text").value = "";
-document.getElementById("amount").value = "";
+amount.value = "";
+
+amount.focus();
 
 }
 
@@ -32,23 +40,41 @@ let list = document.getElementById("list");
 list.innerHTML = "";
 
 let balance = 0;
+let income = 0;
+let expense = 0;
 
 transactions.forEach(t => {
 
 let sign = t.amount < 0 ? "-" : "+";
 balance += t.amount;
 
+if(t.amount > 0){
+income += t.amount;
+}else{
+expense += Math.abs(t.amount);
+}
+
 list.innerHTML += `
 <li>
-${t.text} <span>${sign}₹${Math.abs(t.amount)}</span>
-<button onclick="deleteTransaction(${t.id})">X</button>
+<div>
+<b>${t.text}</b>
+</div>
+
+<div>
+${sign}₹${Math.abs(t.amount)}
+<button onclick="deleteTransaction(${t.id})">❌</button>
+</div>
+
 </li>
 `;
 
 });
 
-document.querySelector(".balance h2").innerText =
-"Balance: ₹" + balance;
+document.getElementById("balance").innerText = "Balance: ₹" + balance;
+document.getElementById("income").innerText = income;
+document.getElementById("expense").innerText = expense;
+
+updateChart();
 
 }
 
@@ -77,3 +103,56 @@ updateUI();
 }
 
 loadFromLocalStorage();
+
+function updateChart() {
+
+let ctx = document.getElementById("myChart").getContext("2d");
+
+if(myChart){
+myChart.destroy();
+}
+
+let categoryTotals = {};
+
+transactions.forEach(t => {
+
+if(!categoryTotals[t.text]){
+categoryTotals[t.text] = 0;
+}
+
+categoryTotals[t.text] += Math.abs(t.amount);
+
+});
+
+let labels = Object.keys(categoryTotals);
+let amounts = Object.values(categoryTotals);
+
+myChart = new Chart(ctx, {
+
+type: "pie",
+
+data: {
+
+labels: labels,
+
+datasets: [{
+
+data: amounts,
+
+backgroundColor: [
+"green",
+"red",
+"blue",
+"orange",
+"purple",
+"yellow",
+"pink"
+]
+
+}]
+
+}
+
+});
+
+}
